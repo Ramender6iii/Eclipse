5feee4a0e440340b8d2830c2fb8a45d9a86fab74
User <User@QA-NB-026>
2024-11-26 18:31:23 +0800

    up

--- TapopSoftware/src/test/java/dashboard_home_TP02/TP02_HP001.java
+++ TapopSoftware/src/test/java/dashboard_home_TP02/TP02_HP001.java
@@ -1,6 +1,7 @@
 package dashboard_home_TP02;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,10 +24,19 @@ public class TP02_HP001 {
 	            driver.get("http://192.168.5.179/");
 
 	            System.out.println(driver.getTitle());
+	            JavascriptExecutor js = (JavascriptExecutor) driver;
+	            String cookies = (String) js.executeScript("return document.cookie;");
+	            System.out.println("Cookies: " + cookies);
+	            
+	            
+	            WebElement link = driver.findElement(By.xpath("//*[@id='includedContent']/div/div[4]/ul/li[1]/a"));
+	            String hrefValue = link.getAttribute("href");
+	         // Print the value of the 'href' attribute
+	            System.out.println("The href attribute value is: " + hrefValue);
 	            
 	            WebElement img = driver.findElement(By.xpath("/html/body/nav/a/img")); 
 	            
-	         // Check if the image is displayed
+	           // Check if the image is displayed
 	            boolean isDisplayed = img.isDisplayed();
 
 	            // Print the result
@@ -36,8 +46,8 @@ public class TP02_HP001 {
 	            System.out.println("Top Image title: The image is not displayed.");
 	            }
 
-
-
+	      //      WebElement nav = driver.findElement(By.xpath("//*[@class='nav-item']"));
+	        //    System.out.println(driver.getTitle());
 	                // Close the driver when done
 	                driver.quit();
 	            }
