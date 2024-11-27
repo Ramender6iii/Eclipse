539717193eca888388b49945b13589b2f645802c
User <User@QA-NB-026>
2024-11-27 10:32:27 +0800

    1

--- TapopSoftware/src/test/java/dashboard_home_TP02/TP02_HP001.java
+++ TapopSoftware/src/test/java/dashboard_home_TP02/TP02_HP001.java
@@ -1,5 +1,7 @@
 package dashboard_home_TP02;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -24,6 +26,7 @@ public class TP02_HP001 {
 	            driver.get("http://192.168.5.179/");
 
 	            System.out.println(driver.getTitle());
+	            
 	            JavascriptExecutor js = (JavascriptExecutor) driver;
 	            String cookies = (String) js.executeScript("return document.cookie;");
 	            System.out.println("Cookies: " + cookies);
@@ -31,7 +34,8 @@ public class TP02_HP001 {
 	            
 	            WebElement link = driver.findElement(By.xpath("//*[@id='includedContent']/div/div[4]/ul/li[1]/a"));
 	            String hrefValue = link.getAttribute("href");
-	         // Print the value of the 'href' attribute
+	            
+	            // Print the value of the 'href' attribute
 	            System.out.println("The href attribute value is: " + hrefValue);
 	            
 	            WebElement img = driver.findElement(By.xpath("/html/body/nav/a/img")); 
@@ -46,10 +50,19 @@ public class TP02_HP001 {
 	            System.out.println("Top Image title: The image is not displayed.");
 	            }
 
-	      //      WebElement nav = driver.findElement(By.xpath("//*[@class='nav-item']"));
-	        //    System.out.println(driver.getTitle());
-	                // Close the driver when done
-	                driver.quit();
+	          
+	          // Find all 'a' elements within 'nav-item' class
+	          List<WebElement> navLinks = driver.findElements(By.xpath("//li[@class='nav-item']/a"));
+
+	          // Check if each element is enabled and print the result
+	          for (WebElement link1 : navLinks) {
+	          boolean isEnabled = link1.isEnabled();
+	          //System.out.println("Nav item: " + link1.getText() + " | Href: " + link1.getAttribute("href") + " | is enabled: " + isEnabled);
+	          System.out.println("Nav item: " + link1.getText() + " is enabled: " + isEnabled);
+	          }
+	          	                    	
+	          // Close the driver when done
+	          driver.quit();
 	            }
 	        }
 		
